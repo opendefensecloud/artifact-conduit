0a2a6d445529167c7c243edb888abd821b254d95
Jan Steffen <jan.steffen@bwi.de>
2025-11-06 15:19:34 +0100

    adjust

--- devenv.nix
+++ devenv.nix
@@ -16,36 +16,15 @@
   languages.go.enable = true;
   languages.go.package = pkgs.go_1_25;
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  # enterTest = ''
-  #   echo "Running tests"
-  #   git --version | grep --color=auto "${pkgs.git.version}"
-  # '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
+  git-hooks.hooks = {
+    govet = {
+      enable = true;
+      pass_filenames = false;
+    };
+    golangci-lint = {
+      enable = true;
+      pass_filenames = false;
+    };
+  };
   # See full reference at https://devenv.sh/reference/options/
 }
