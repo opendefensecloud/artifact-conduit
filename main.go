afa138591da9cadd3a3b3b8ee4ab8f3f374d5180
Jan Steffen <jan.steffen@bwi.de>
2025-11-06 14:13:29 +0100

    refactor

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "gitlab.opencode.de/bwi/ace/artifact-conduit/cmd"
+import "gitlab.opencode.de/bwi/ace/artifact-conduit/cmd/arcctl"
 
 func main() {
 	cmd.Execute()
