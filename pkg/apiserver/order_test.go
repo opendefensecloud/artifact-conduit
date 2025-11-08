ab087ddc2f6016bb3f2fad62a76581c5a3e82fc5
Niklas Voss <niklas.voss@gmail.com>
2025-11-08 10:39:49 +0100

    extend test to verify whether any spec can be set

--- pkg/apiserver/order_test.go
+++ pkg/apiserver/order_test.go
@@ -6,6 +6,8 @@ import (
 	. "github.com/onsi/gomega"
 	orderv1alpha1 "gitlab.opencode.de/bwi/ace/artifact-conduit/api/order/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Order", func() {
@@ -18,14 +20,21 @@ var _ = Describe("Order", func() {
 	Context("Order", func() {
 		It("should allow creating an order", func() {
 			By("creating a test order")
+			expectedRaw := []byte(`{"oci":[]}`)
 			order = &orderv1alpha1.Order{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace:    ns.Name,
 					GenerateName: "test-",
 				},
-				Spec: orderv1alpha1.OrderSpec{},
+				Spec: orderv1alpha1.OrderSpec{
+					RawExtension: runtime.RawExtension{
+						Raw: expectedRaw,
+					},
+				},
 			}
 			Expect(k8sClient.Create(ctx, order)).To(Succeed())
+			Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(order), order)).To(Succeed())
+			Expect(order.Spec.Raw).To(Equal(expectedRaw))
 		})
 		It("should allow deleting an order", func() {
 			By("deleting a test order")
