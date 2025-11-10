3c74043477b21a1c222382837feae958019effde
Jan Steffen <jan.steffen@bwi.de>
2025-11-10 14:48:31 +0100

    fix test

--- cmd/arcctl/oci/pull_test.go
+++ cmd/arcctl/oci/pull_test.go
@@ -1,16 +1,17 @@
 package oci
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func TestRunPullGinkgo(t *testing.T) {
-	gomega.RegisterFailHandler(Fail)
+	RegisterFailHandler(Fail)
 	RunSpecs(t, "arcctl oci pull Suite")
 }
 
@@ -18,7 +19,9 @@ var _ = Describe("Pull Command", func() {
 	var (
 		cmd *cobra.Command
 	)
-
+	const (
+		arcctlTempDir = "/tmp/arcctl-tests"
+	)
 	BeforeEach(func() {
 		cmd = NewPullCommand()
 		cmd.SetContext(GinkgoT().Context())
@@ -26,26 +29,30 @@ var _ = Describe("Pull Command", func() {
 
 	AfterEach(func() {
 		viper.Reset()
+		Expect(os.RemoveAll(arcctlTempDir)).ToNot(HaveOccurred())
 	})
 
 	Context("when required configuration is missing", func() {
 
 		It("should return an error if source.reference is missing", func() {
 			err := runPull(cmd, []string{})
-			gomega.Expect(err).To(gomega.HaveOccurred())
-			gomega.Expect(err.Error()).To(gomega.ContainSubstring("source.reference is not set"))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("source.reference is not set"))
 		})
 	})
 
 	Context("when configuration is valid", func() {
 		BeforeEach(func() {
 			viper.Set("source.reference", "registry-1.docker.io/library/busybox:latest")
-			viper.Set("tmp-dir", "tmp/test")
+			viper.Set("tmp-dir", arcctlTempDir)
 		})
 
 		It("should pull the OCI artifact successfully", func() {
 			err := runPull(cmd, []string{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			Expect(err).ToNot(HaveOccurred())
+			// Verify oci layout on disk exists
+			_, err = os.Stat(arcctlTempDir + "/index.json")
+			Expect(err).ToNot(HaveOccurred())
 		})
 
 	})
