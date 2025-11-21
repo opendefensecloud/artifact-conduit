877e48f41f52142c29b828570c1fb8f66f0b7d79
Niklas Voss <niklas.voss@bwi.de>
2025-11-21 14:51:35 +0100

    run make fmt

--- test/e2e/doc.go
+++ test/e2e/doc.go
@@ -1,3 +1,6 @@
+// Copyright 2025 BWI GmbH and Artifact Conduit contributors
+// SPDX-License-Identifier: Apache-2.0
+
 // This package requires a build constraint. Make sure the environment, where the
 // test-suite is run, has sufficient resources.
 //
