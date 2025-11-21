a433f2b5896683c305963dddbc8622a562b36a90
Niklas Voss <niklas.voss@bwi.de>
2025-11-21 14:29:07 +0100

    add doc.go to test/e2e to avoid errors in CI.

---
+++ test/e2e/doc.go
@@ -0,0 +1,5 @@
+// This package requires a build constraint. Make sure the environment, where the
+// test-suite is run, has sufficient resources.
+//
+// Use `make test-e2e` to run the test-suite.
+package e2e
