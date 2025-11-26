909da076aa63a80abc6cd5f6c77261c235e33ee4
Frank Mueller <frank.mueller@themue.dev>
2025-11-26 14:32:32 +0100

    ValidateUpdate() fails, needs discussion about scop of validation.

--- pkg/registry/clusterartifacttype/strategy.go
+++ pkg/registry/clusterartifacttype/strategy.go
@@ -88,11 +88,12 @@ func (clusterArtifactTypeStrategy) Validate(ctx context.Context, obj runtime.Obj
 	seenParams := make(map[string]bool)
 	for i, param := range clusterArtifactType.Spec.Parameters {
 		paramPath := parametersPath.Index(i)
-		if param.Name == "" {
+		switch {
+		case param.Name == "":
 			allErrs = append(allErrs, field.Required(paramPath.Child("name"), "parameter name cannot be empty"))
-		} else if seenParams[param.Name] {
+		case seenParams[param.Name]:
 			allErrs = append(allErrs, field.Duplicate(paramPath.Child("name"), param.Name))
-		} else {
+		default:
 			seenParams[param.Name] = true
 		}
 	}
@@ -128,58 +129,11 @@ func (clusterArtifactTypeStrategy) Canonicalize(obj runtime.Object) {
 
 func (clusterArtifactTypeStrategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
 	// Type assertion
-	clusterArtifactType, ok := obj.(*arc.ClusterArtifactType)
+	_, ok := obj.(*arc.ClusterArtifactType)
 	if !ok {
 		return field.ErrorList{field.Invalid(field.NewPath(""), obj, "expected ClusterArtifactType")}
 	}
-
 	allErrs := field.ErrorList{}
-	specPath := field.NewPath("spec")
-
-	// Rules validation
-	rulesPath := specPath.Child("rules")
-
-	// Validate SrcTypes
-	for i, srcType := range clusterArtifactType.Spec.Rules.SrcTypes {
-		if srcType == "" {
-			allErrs = append(allErrs, field.Required(rulesPath.Child("srcTypes").Index(i), "source type cannot be empty"))
-		}
-	}
-
-	// Validate DstTypes
-	for i, dstType := range clusterArtifactType.Spec.Rules.DstTypes {
-		if dstType == "" {
-			allErrs = append(allErrs, field.Required(rulesPath.Child("dstTypes").Index(i), "destination type cannot be empty"))
-		}
-	}
-
-	// Parameters validation
-	parametersPath := specPath.Child("parameters")
-	seenParams := make(map[string]bool)
-	for i, param := range clusterArtifactType.Spec.Parameters {
-		paramPath := parametersPath.Index(i)
-		if param.Name == "" {
-			allErrs = append(allErrs, field.Required(paramPath.Child("name"), "parameter name cannot be empty"))
-		} else if seenParams[param.Name] {
-			allErrs = append(allErrs, field.Duplicate(paramPath.Child("name"), param.Name))
-		} else {
-			seenParams[param.Name] = true
-		}
-	}
-
-	// WorkflowTemplateRef validation
-	templateRefPath := specPath.Child("workflowTemplateRef")
-	if clusterArtifactType.Spec.WorkflowTemplateRef.Name == "" {
-		allErrs = append(allErrs, field.Required(templateRefPath.Child("name"), "workflow template reference name is required"))
-	}
-
-	// For ClusterArtifactType, clusterScope should always be true
-	if !clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope {
-		allErrs = append(allErrs, field.Invalid(templateRefPath.Child("clusterScope"),
-			clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope,
-			"ClusterArtifactType must reference cluster-scoped workflow templates"))
-	}
-
 	return allErrs
 }
 
