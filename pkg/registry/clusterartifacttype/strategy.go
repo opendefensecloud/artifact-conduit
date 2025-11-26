702afb2443184e5fd7218479a5da2e52a789c34a
Frank Mueller <frank.mueller@themue.dev>
2025-11-26 14:20:50 +0100

    Start changes for artifact immutability

--- pkg/registry/clusterartifacttype/strategy.go
+++ pkg/registry/clusterartifacttype/strategy.go
@@ -63,7 +63,51 @@ func (clusterArtifactTypeStrategy) PrepareForUpdate(ctx context.Context, obj, ol
 }
 
 func (clusterArtifactTypeStrategy) Validate(ctx context.Context, obj runtime.Object) field.ErrorList {
-	return field.ErrorList{}
+	// Type assertion
+	clusterArtifactType, ok := obj.(*arc.ClusterArtifactType)
+	if !ok {
+		return field.ErrorList{field.Invalid(field.NewPath(""), obj, "expected ClusterArtifactType")}
+	}
+	// Individual validations
+	allErrs := field.ErrorList{}
+	specPath := field.NewPath("spec")
+	rulesPath := specPath.Child("rules")
+	// Validate SrcTypes and DstTypes
+	for i, srcType := range clusterArtifactType.Spec.Rules.SrcTypes {
+		if srcType == "" {
+			allErrs = append(allErrs, field.Required(rulesPath.Child("srcTypes").Index(i), "source type cannot be empty"))
+		}
+	}
+	for i, dstType := range clusterArtifactType.Spec.Rules.DstTypes {
+		if dstType == "" {
+			allErrs = append(allErrs, field.Required(rulesPath.Child("dstTypes").Index(i), "destination type cannot be empty"))
+		}
+	}
+	// Validate parameters
+	parametersPath := specPath.Child("parameters")
+	seenParams := make(map[string]bool)
+	for i, param := range clusterArtifactType.Spec.Parameters {
+		paramPath := parametersPath.Index(i)
+		if param.Name == "" {
+			allErrs = append(allErrs, field.Required(paramPath.Child("name"), "parameter name cannot be empty"))
+		} else if seenParams[param.Name] {
+			allErrs = append(allErrs, field.Duplicate(paramPath.Child("name"), param.Name))
+		} else {
+			seenParams[param.Name] = true
+		}
+	}
+	// Validate WorkflowTemplateRef
+	templateRefPath := specPath.Child("workflowTemplateRef")
+	if clusterArtifactType.Spec.WorkflowTemplateRef.Name == "" {
+		allErrs = append(allErrs, field.Required(templateRefPath.Child("name"), "workflow template reference name is required"))
+	}
+	// Validate ClusterScope to be true for ClusterArtifactType
+	if !clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope {
+		allErrs = append(allErrs, field.Invalid(templateRefPath.Child("clusterScope"),
+			clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope,
+			"ClusterArtifactType must reference cluster-scoped workflow templates"))
+	}
+	return allErrs
 }
 
 // WarningsOnCreate returns warnings for the creation of the given object.
@@ -83,7 +127,60 @@ func (clusterArtifactTypeStrategy) Canonicalize(obj runtime.Object) {
 }
 
 func (clusterArtifactTypeStrategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
-	return field.ErrorList{}
+	// Type assertion
+	clusterArtifactType, ok := obj.(*arc.ClusterArtifactType)
+	if !ok {
+		return field.ErrorList{field.Invalid(field.NewPath(""), obj, "expected ClusterArtifactType")}
+	}
+
+	allErrs := field.ErrorList{}
+	specPath := field.NewPath("spec")
+
+	// Rules validation
+	rulesPath := specPath.Child("rules")
+
+	// Validate SrcTypes
+	for i, srcType := range clusterArtifactType.Spec.Rules.SrcTypes {
+		if srcType == "" {
+			allErrs = append(allErrs, field.Required(rulesPath.Child("srcTypes").Index(i), "source type cannot be empty"))
+		}
+	}
+
+	// Validate DstTypes
+	for i, dstType := range clusterArtifactType.Spec.Rules.DstTypes {
+		if dstType == "" {
+			allErrs = append(allErrs, field.Required(rulesPath.Child("dstTypes").Index(i), "destination type cannot be empty"))
+		}
+	}
+
+	// Parameters validation
+	parametersPath := specPath.Child("parameters")
+	seenParams := make(map[string]bool)
+	for i, param := range clusterArtifactType.Spec.Parameters {
+		paramPath := parametersPath.Index(i)
+		if param.Name == "" {
+			allErrs = append(allErrs, field.Required(paramPath.Child("name"), "parameter name cannot be empty"))
+		} else if seenParams[param.Name] {
+			allErrs = append(allErrs, field.Duplicate(paramPath.Child("name"), param.Name))
+		} else {
+			seenParams[param.Name] = true
+		}
+	}
+
+	// WorkflowTemplateRef validation
+	templateRefPath := specPath.Child("workflowTemplateRef")
+	if clusterArtifactType.Spec.WorkflowTemplateRef.Name == "" {
+		allErrs = append(allErrs, field.Required(templateRefPath.Child("name"), "workflow template reference name is required"))
+	}
+
+	// For ClusterArtifactType, clusterScope should always be true
+	if !clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope {
+		allErrs = append(allErrs, field.Invalid(templateRefPath.Child("clusterScope"),
+			clusterArtifactType.Spec.WorkflowTemplateRef.ClusterScope,
+			"ClusterArtifactType must reference cluster-scoped workflow templates"))
+	}
+
+	return allErrs
 }
 
 // WarningsOnUpdate returns warnings for the given update.
