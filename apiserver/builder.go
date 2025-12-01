0a7f258640f8bbfc14cc2a92604515f56d062400
Niklas Voss <niklas.voss@bwi.de>
2025-12-01 16:13:30 +0100

    move creation of apiserver into builder pattern

---
+++ apiserver/builder.go
@@ -0,0 +1,260 @@
+package apiserver
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/version"
+	"k8s.io/apiserver/pkg/admission"
+	"k8s.io/apiserver/pkg/endpoints/openapi"
+	genericapiserver "k8s.io/apiserver/pkg/server"
+	genericoptions "k8s.io/apiserver/pkg/server/options"
+	"k8s.io/apiserver/pkg/util/compatibility"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	"k8s.io/component-base/cli"
+	basecompatibility "k8s.io/component-base/compatibility"
+	"k8s.io/component-base/featuregate"
+	baseversion "k8s.io/component-base/version"
+	openapicommon "k8s.io/kube-openapi/pkg/common"
+	netutils "k8s.io/utils/net"
+)
+
+type ExtraAdmissionInitializers func(*genericapiserver.RecommendedConfig) (SharedInformerFactory, []admission.PluginInitializer, error)
+type RecommendedConfigFn func(*genericapiserver.RecommendedConfig)
+
+type SharedInformerFactory interface {
+	Start(stopCh <-chan struct{})
+}
+
+type APIGroupFn func(scheme *runtime.Scheme, codecs serializer.CodecFactory, c *genericapiserver.CompletedConfig) genericapiserver.APIGroupInfo
+
+type Builder struct {
+	componentName                          string
+	groupName                              string
+	alternateDNS                           []string
+	scheme                                 *runtime.Scheme
+	codecs                                 serializer.CodecFactory
+	orderedGroupVersions                   []schema.GroupVersion
+	skipDefaultComponentGlobalsRegistrySet bool
+	extraAdmissionInitializers             ExtraAdmissionInitializers
+	sharedInformerFactories                []SharedInformerFactory
+	recommendedOptions                     *genericoptions.RecommendedOptions
+	componentGlobalsRegistry               basecompatibility.ComponentGlobalsRegistry
+	recommendedConfigFns                   []RecommendedConfigFn
+	apiGroupFns                            []APIGroupFn
+}
+
+func NewBuilder(scheme *runtime.Scheme) *Builder {
+	return &Builder{
+		scheme:                  scheme,
+		codecs:                  serializer.NewCodecFactory(scheme),
+		sharedInformerFactories: []SharedInformerFactory{},
+		apiGroupFns:             []APIGroupFn{},
+	}
+}
+
+func (b *Builder) WithComponentName(n string) *Builder {
+	b.componentName = n
+	return b
+}
+
+func (b *Builder) WithGroupName(n string) *Builder {
+	b.groupName = n
+	return b
+}
+
+func (b *Builder) WithOpenAPIDefinitions(name, version string, defs openapicommon.GetOpenAPIDefinitions) *Builder {
+	b.recommendedConfigFns = append(b.recommendedConfigFns, func(config *genericapiserver.RecommendedConfig) {
+		config.OpenAPIConfig = genericapiserver.DefaultOpenAPIConfig(defs, openapi.NewDefinitionNamer(b.scheme))
+		config.OpenAPIConfig.Info.Title = name
+		config.OpenAPIConfig.Info.Version = version
+
+		config.OpenAPIV3Config = genericapiserver.DefaultOpenAPIV3Config(defs, openapi.NewDefinitionNamer(b.scheme))
+		config.OpenAPIV3Config.Info.Title = name
+		config.OpenAPIV3Config.Info.Version = name
+	})
+	return b
+}
+
+func (b *Builder) WithAPIGroupFn(fn APIGroupFn) *Builder {
+	if fn == nil {
+		return b
+	}
+	b.apiGroupFns = append(b.apiGroupFns, fn)
+	return b
+}
+
+func (b *Builder) WithExtraAdmissionInitializers(f ExtraAdmissionInitializers) *Builder {
+	if f == nil {
+		return b
+	}
+	b.extraAdmissionInitializers = f
+	return b
+}
+
+func (b *Builder) WithSharedInformerFactory(f SharedInformerFactory) *Builder {
+	if f == nil {
+		return b
+	}
+	b.sharedInformerFactories = append(b.sharedInformerFactories, f)
+	return b
+}
+
+// WithOrderedGroupVersions sets the ordered group versions which are used
+// to configure storage encoding/decoding for the API server. This must be
+// provided by callers so that the storage codec matches the registered types
+// in the scheme.
+func (b *Builder) WithOrderedGroupVersions(gvs []schema.GroupVersion) *Builder {
+	b.orderedGroupVersions = gvs
+	return b
+}
+
+func (b *Builder) Execute() int {
+	if b.recommendedOptions == nil {
+		b.recommendedOptions = genericoptions.NewRecommendedOptions(
+			fmt.Sprintf("/registry/%s", b.groupName),
+			b.codecs.LegacyCodec(b.orderedGroupVersions...),
+		)
+	}
+	b.recommendedOptions.Etcd.StorageConfig.EncodeVersioner = schema.GroupVersions(b.orderedGroupVersions)
+	if b.extraAdmissionInitializers != nil {
+		b.recommendedOptions.ExtraAdmissionInitializers = func(c *genericapiserver.RecommendedConfig) ([]admission.PluginInitializer, error) {
+			informerFactory, pluginInitialisers, err := b.extraAdmissionInitializers(c)
+			if err != nil {
+				return nil, err
+			}
+			b.sharedInformerFactories = append(b.sharedInformerFactories, informerFactory)
+			return pluginInitialisers, nil
+		}
+	}
+	utilruntime.Must(b.recommendedOptions.SecureServing.MaybeDefaultWithSelfSignedCerts("localhost", b.alternateDNS, []net.IP{netutils.ParseIPSloppy("127.0.0.1")}))
+
+	if b.componentGlobalsRegistry == nil {
+		b.componentGlobalsRegistry = compatibility.DefaultComponentGlobalsRegistry
+	}
+
+	ctx := genericapiserver.SetupSignalContext()
+	cmd := &cobra.Command{
+		Short: "Launch API server",
+		Long:  "Launch API server",
+		PersistentPreRunE: func(*cobra.Command, []string) error {
+			if b.skipDefaultComponentGlobalsRegistrySet {
+				return nil
+			}
+			return b.componentGlobalsRegistry.Set()
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			// Validate essential builder configuration early to provide a helpful error
+			if len(b.orderedGroupVersions) == 0 {
+				return fmt.Errorf("orderedGroupVersions not set on Builder; call WithOrderedGroupVersions(...) before Execute")
+			}
+			errors := []error{}
+			errors = append(errors, b.recommendedOptions.Validate()...)
+			errors = append(errors, b.componentGlobalsRegistry.Validate()...)
+			if err := utilerrors.NewAggregate(errors); err != nil {
+				return err
+			}
+
+			serverConfig := genericapiserver.NewRecommendedConfig(b.codecs)
+
+			for _, fn := range b.recommendedConfigFns {
+				fn(serverConfig)
+			}
+
+			serverConfig.FeatureGate = b.componentGlobalsRegistry.FeatureGateFor(basecompatibility.DefaultKubeComponent)
+			serverConfig.EffectiveVersion = b.componentGlobalsRegistry.EffectiveVersionFor(b.componentName)
+
+			if err := b.recommendedOptions.ApplyTo(serverConfig); err != nil {
+				return err
+			}
+
+			completedConfig := serverConfig.Complete()
+			server, err := completedConfig.New(fmt.Sprintf("%s-apiserver", b.componentName), genericapiserver.NewEmptyDelegate())
+			if err != nil {
+				return err
+			}
+
+			// TODO: install API groups with their storage backends!
+			for _, fn := range b.apiGroupFns {
+				apiGroupInfo := fn(b.scheme, b.codecs, &completedConfig)
+				if err := server.InstallAPIGroup(&apiGroupInfo); err != nil {
+					return err
+				}
+
+			}
+
+			server.AddPostStartHookOrDie(fmt.Sprintf("start-%s-server-informers", b.componentName), func(context genericapiserver.PostStartHookContext) error {
+				// Defensive: the SharedInformerFactory may not be set by the recommended options
+				// in all call sites (callers may provide their own factories via WithSharedInformerFactory).
+				// Avoid a nil-pointer panic by checking for nil before starting.
+				if serverConfig.SharedInformerFactory != nil {
+					serverConfig.SharedInformerFactory.Start(context.Done())
+				}
+				for _, sharedInformerFactory := range b.sharedInformerFactories {
+					sharedInformerFactory.Start(context.Done())
+				}
+				return nil
+			})
+
+			return server.PrepareRun().RunWithContext(ctx)
+		},
+	}
+	cmd.SetContext(ctx)
+
+	flags := cmd.Flags()
+	b.recommendedOptions.AddFlags(flags)
+
+	// TODO: expose to builder
+	defaultVersion := "1.2"
+	// Register the "ARC" component with the global component registry,
+	// associating it with its effective version and feature gate configuration.
+	// Will skip if the component has been registered, like in the integration test.
+	_, _ = b.componentGlobalsRegistry.ComponentGlobalsOrRegister(
+		b.componentName, basecompatibility.NewEffectiveVersionFromString(defaultVersion, "", ""),
+		featuregate.NewVersionedFeatureGate(version.MustParse(defaultVersion)))
+
+	// Add versioned feature specifications for the "BanFlunder" feature.
+	// These specifications, together with the effective version, determine if the feature is enabled.
+	// TODO: expose to builder
+	// utilruntime.Must(arcFeatureGate.AddVersioned(map[featuregate.Feature]featuregate.VersionedSpecs{
+	// 	"BanFlunder": {
+	// 		{Version: version.MustParse("1.0"), Default: false, PreRelease: featuregate.Alpha},
+	// 		{Version: version.MustParse("1.1"), Default: true, PreRelease: featuregate.Beta},
+	// 		{Version: version.MustParse("1.2"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
+	// 	},
+	// }))
+
+	// Register the default kube component if not already present in the global registry.
+	_, _ = b.componentGlobalsRegistry.ComponentGlobalsOrRegister(basecompatibility.DefaultKubeComponent,
+		basecompatibility.NewEffectiveVersionFromString(baseversion.DefaultKubeBinaryVersion, "", ""), utilfeature.DefaultMutableFeatureGate)
+
+	// Set the emulation version mapping from the "ARC" component to the kube component.
+	// This ensures that the emulation version of the latter is determined by the emulation version of the former.
+
+	versionToKubeVersion := func(ver *version.Version) *version.Version {
+		if ver.Major() != 1 {
+			return nil
+		}
+		kubeVer := version.MustParse(baseversion.DefaultKubeBinaryVersion)
+		// "1.2" maps to kubeVer
+		offset := int(ver.Minor()) - 2
+		mappedVer := kubeVer.OffsetMinor(offset)
+		if mappedVer.GreaterThan(kubeVer) {
+			return kubeVer
+		}
+		return mappedVer
+	}
+	utilruntime.Must(b.componentGlobalsRegistry.SetEmulationVersionMapping(b.componentName, basecompatibility.DefaultKubeComponent, versionToKubeVersion))
+
+	b.componentGlobalsRegistry.AddFlags(flags)
+
+	// TODO: add kube version compatibility matrix and feature gates
+
+	return cli.Run(cmd)
+}
--- cmd/arc-apiserver/main.go
+++ cmd/arc-apiserver/main.go
@@ -6,15 +6,86 @@ package main
 import (
 	"os"
 
-	"go.opendefense.cloud/arc/pkg/apiserver"
-	genericapiserver "k8s.io/apiserver/pkg/server"
-	"k8s.io/component-base/cli"
+	"go.opendefense.cloud/arc/api/arc"
+	"go.opendefense.cloud/arc/api/arc/install"
+	arcv1alpha1 "go.opendefense.cloud/arc/api/arc/v1alpha1"
+	"go.opendefense.cloud/arc/apiserver"
+	clientset "go.opendefense.cloud/arc/client-go/clientset/versioned"
+	informers "go.opendefense.cloud/arc/client-go/informers/externalversions"
+	"go.opendefense.cloud/arc/client-go/openapi"
+	"go.opendefense.cloud/arc/pkg/admission/orderinitializer"
+	arcregistry "go.opendefense.cloud/arc/pkg/registry"
+	artifacttypestorage "go.opendefense.cloud/arc/pkg/registry/artifacttype"
+	artifactworkflowstorage "go.opendefense.cloud/arc/pkg/registry/artifactworkflow"
+	clusterartifacttypestorage "go.opendefense.cloud/arc/pkg/registry/clusterartifacttype"
+	endpointstorage "go.opendefense.cloud/arc/pkg/registry/endpoint"
+	orderstorage "go.opendefense.cloud/arc/pkg/registry/order"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apiserver/pkg/admission"
+	"k8s.io/apiserver/pkg/registry/rest"
+	"k8s.io/apiserver/pkg/server"
 )
 
+const (
+	componentName = "arc"
+)
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	install.Install(scheme)
+
+	// we need to add the options to empty v1
+	// TODO: fix the server code to avoid this
+	metav1.AddToGroupVersion(scheme, schema.GroupVersion{Version: "v1"})
+
+	// TODO: keep the generic API server from wanting this
+	unversioned := schema.GroupVersion{Group: "", Version: "v1"}
+	scheme.AddUnversionedTypes(unversioned,
+		&metav1.Status{},
+		&metav1.APIVersions{},
+		&metav1.APIGroupList{},
+		&metav1.APIGroup{},
+		&metav1.APIResourceList{},
+	)
+}
 func main() {
-	ctx := genericapiserver.SetupSignalContext()
-	options := apiserver.NewARCServerOptions(os.Stdout, os.Stderr)
-	cmd := apiserver.NewCommandStartARCServer(ctx, options, false)
-	code := cli.Run(cmd)
+	code := apiserver.NewBuilder(scheme).
+		WithComponentName(componentName).
+		WithGroupName(arc.GroupName).
+		WithOpenAPIDefinitions(componentName, "v0.1.0", openapi.GetOpenAPIDefinitions).
+		WithExtraAdmissionInitializers(func(c *server.RecommendedConfig) (apiserver.SharedInformerFactory, []admission.PluginInitializer, error) {
+			client, err := clientset.NewForConfig(c.LoopbackClientConfig)
+			if err != nil {
+				return nil, nil, err
+			}
+			informerFactory := informers.NewSharedInformerFactory(client, c.LoopbackClientConfig.Timeout)
+			return informerFactory, []admission.PluginInitializer{orderinitializer.New(informerFactory)}, nil
+		}).
+		// TODO: refactor how we construct api groups and storage registries
+		WithAPIGroupFn(func(scheme *runtime.Scheme, codecs serializer.CodecFactory, c *server.CompletedConfig) server.APIGroupInfo {
+			apiGroupInfo := server.NewDefaultAPIGroupInfo(arc.GroupName, scheme, metav1.ParameterCodec, codecs)
+
+			v1alpha1storage := map[string]rest.Storage{}
+			v1alpha1storage["orders"] = arcregistry.RESTInPeace(orderstorage.NewREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["orders/status"] = arcregistry.RESTInPeace(orderstorage.NewStatusREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["artifactworkflows"] = arcregistry.RESTInPeace(artifactworkflowstorage.NewREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["artifactworkflows/status"] = arcregistry.RESTInPeace(artifactworkflowstorage.NewStatusREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["endpoints"] = arcregistry.RESTInPeace(endpointstorage.NewREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["artifacttypes"] = arcregistry.RESTInPeace(artifacttypestorage.NewREST(scheme, c.RESTOptionsGetter))
+			v1alpha1storage["clusterartifacttypes"] = arcregistry.RESTInPeace(clusterartifacttypestorage.NewREST(scheme, c.RESTOptionsGetter))
+			apiGroupInfo.VersionedResourcesStorageMap["v1alpha1"] = v1alpha1storage
+
+			return apiGroupInfo
+		}).
+		WithOrderedGroupVersions([]schema.GroupVersion{
+			arcv1alpha1.SchemeGroupVersion,
+		}).
+		Execute()
 	os.Exit(code)
 }
