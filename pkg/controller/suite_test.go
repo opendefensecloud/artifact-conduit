c97fdb056596e9db4e6acab4cabf78c96b24539d
Doreen Seider <doreen@seider.berlin>
2026-04-15 16:32:02 +0200

    chore: fix linter issues that appeared after Go version update

--- pkg/controller/suite_test.go
+++ pkg/controller/suite_test.go
@@ -17,7 +17,6 @@ import (
 	testclient "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/events"
-	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlconfig "sigs.k8s.io/controller-runtime/pkg/config"
@@ -96,7 +95,7 @@ var _ = BeforeSuite(func() {
 		Metrics: metricserver.Options{
 			BindAddress: "0",
 		},
-		Controller: ctrlconfig.Controller{SkipNameValidation: ptr.To(true)},
+		Controller: ctrlconfig.Controller{SkipNameValidation: new(true)},
 	})
 	Expect(err).ToNot(HaveOccurred())
 
