d48cc31c9ae26192eeb9f6655aa34a8e08f9b8a4
Alexander Olzem <alexander.olzem@opendefense.cloud>
2026-04-23 15:42:11 +0200

    fix: use explicit input for flake-utils

--- flake.nix
+++ flake.nix
@@ -3,20 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     go-overlay = {
       url = "github:purpleclay/go-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     dev-kit = {
       url = "github:opendefensecloud/dev-kit";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.go-overlay.follows = "go-overlay";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { nixpkgs, flake-utils, dev-kit, ... }@inputs:
+  outputs = { nixpkgs, flake-utils, dev-kit, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
