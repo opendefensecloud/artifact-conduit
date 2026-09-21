f04d5972761e8a4d9a60f30920967ef2429e5b14
Doreen <seider.doreen@gmail.com>
2026-09-21 14:06:46 +0200

    chore: update copyright headers

--- pkg/admission/orderinitializer/orderinitializer_test.go
+++ pkg/admission/orderinitializer/orderinitializer_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 BWI GmbH and Artifact Conduit contributors
+// Copyright BWI GmbH and Artifact Conduit contributors
 // SPDX-License-Identifier: Apache-2.0
 
 package orderinitializer_test
